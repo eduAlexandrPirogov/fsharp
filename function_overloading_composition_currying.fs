d33c660855fa64e688ce489c0232d68fdfc2b09f
Alexander <a.pirogov@td-media.ru>
2022-03-31 14:26:50 +0300

    qwe

--- function_overloading_composition_currying.fs
+++ function_overloading_composition_currying.fs
@@ -1,11 +1,11 @@
 // 20.3.1
-let vat (n:int) = fun (x:float) -> x + x / (100.0 * float(n)) :float
+let vat n x = x + x / 100.0 * float(n) : float
 
 // 20.3.2
-let unvat (n:int) =( fun (x:float) -> float(n) * 100.0/(100.0+x))
+let unvat n x = fun (x:float) -> float(n) * 100.0/(100.0+x)
 
 // 20.3.3
-let rec min (f: int->int) = (fun (n:int) -> 
+let rec min f = (fun (n:int) -> 
     let n = f n
     match (n) with
     | n when f n <= 0 -> n: int
