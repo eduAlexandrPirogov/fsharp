336828a5ac3cecb41114dc10b046de6778ff9b35
Alexander <a.pirogov@td-media.ru>
2022-03-31 19:18:50 +0300

    s

--- function_overloading_composition_currying.fs
+++ function_overloading_composition_currying.fs
@@ -2,10 +2,10 @@
 let vat n x = x + x / 100.0 * float(n) : float
 
 // 20.3.2
-let unvat n x = fun (x:float) -> float(n) * 100.0/(100.0+float(x))
+let unvat n x = float(n) * 100.0/(100.0+float(x))
 
 // 20.3.3
-let rec min (f:int -> int) = 
+let rec min f = 
     let rec subn n = f n
     function
     | n when n <= 1 -> n :int
