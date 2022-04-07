4badf7a814580aa5fdbb60d11a10235350eaef27
Alexander <a.pirogov@td-media.ru>
2022-04-07 10:17:22 +0300

    s

--- operators_kortje.fs
+++ operators_kortje.fs
@@ -22,6 +22,28 @@ let (.+.) x y =
     let silvers_converted = convert_silvers coupers_converted
     silvers_converted
 
+let (.-.) x y = 
+    let ((_,_,c1):int*int*int) = y
+    let ((_,s1,_):int*int*int) = y 
+    let ((g1,_,_):int*int*int) = y
+    let ((_,_,c2):int*int*int) = x
+    let ((_,s2,_):int*int*int) = x 
+    let ((g2,_,_):int*int*int) = x
+
+    let money = (g2-g1, s2-s1, c2-c1)
+
+    let rec convert_coupers = function
+        | (g, s, c) when c <= 11 -> (g,s,c)
+        | (g, s, c) -> convert_coupers (g, s+1, c-12)
+
+    let rec convert_silvers = function
+        | (g, s, c) when s <= 19 -> (g,s,c)
+        | (g, s, c) -> convert_silvers (g+1, s-20, c)
+
+
+    let coupers_converted = convert_coupers money
+    let silvers_converted = convert_silvers coupers_converted
+    silvers_converted
 
 // 23.4.2
 let (.+) x y = 
