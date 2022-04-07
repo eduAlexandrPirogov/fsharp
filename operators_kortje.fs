cee1ed5f0eb9d309a9e59eca83b843a0d041ae57
Alexander <a.pirogov@td-media.ru>
2022-04-07 10:41:46 +0300

    coupesr

--- operators_kortje.fs
+++ operators_kortje.fs
@@ -23,27 +23,35 @@ let (.+.) x y =
     silvers_converted
 
 let (.-.) x y = 
-    let ((_,_,c1):int*int*int) = y
-    let ((_,s1,_):int*int*int) = y 
-    let ((g1,_,_):int*int*int) = y
-    let ((_,_,c2):int*int*int) = x
-    let ((_,s2,_):int*int*int) = x 
-    let ((g2,_,_):int*int*int) = x
-
-    let money = (g2-g1, s2-s1, c2-c1)
-
+  
     let rec convert_coupers = function
         | (g, s, c) when c <= 11 -> (g,s,c)
         | (g, s, c) -> convert_coupers (g, s+1, c-12)
 
+        
     let rec convert_silvers = function
         | (g, s, c) when s <= 19 -> (g,s,c)
         | (g, s, c) -> convert_silvers (g+1, s-20, c)
 
+  
+    let coupers_converted_x = convert_coupers x
+    let silvers_converted_x = convert_silvers coupers_converted_x
 
-    let coupers_converted = convert_coupers money
-    let silvers_converted = convert_silvers coupers_converted
-    silvers_converted
+    
+    let coupers_converted_y = convert_coupers y
+    let silvers_converted_y = convert_silvers coupers_converted_y
+
+    
+    let ((_,_,c1):int*int*int) = silvers_converted_y
+    let ((_,s1,_):int*int*int) = silvers_converted_y 
+    let ((g1,_,_):int*int*int) = silvers_converted_y
+    let ((_,_,c2):int*int*int) = silvers_converted_x
+    let ((_,s2,_):int*int*int) = silvers_converted_x 
+    let ((g2,_,_):int*int*int) = silvers_converted_x
+
+    let money = (g2-g1, s2-s1, c2-c1)
+
+    money
 
 // 23.4.2
 let (.+) x y = 
