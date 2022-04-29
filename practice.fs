4cab10b3f2f7b0aafa835cfb4a7db99651c2ab35
Alexandr Pirogov <88452033+eduAlexandrPirogov@users.noreply.github.com>
2022-04-29 17:11:19 +0300

    Update practice.fs

--- practice.fs
+++ practice.fs
@@ -48,6 +48,7 @@ let rec check_in = function
 // 40.2.3
 let rec intersect (xs1, xs2) = 
     let rec fill_intersect = function
+        | ([], xs2, res) -> []
         | (head1::tail1, xs2, res) when tail1 = [] && check_in (xs2,head1) = false -> res //Если нет во втором списк вообще
         | (head1::tail1, xs2, res) when tail1 = [] && check_in (xs2,head1) = true && check_in (res, head1) = false-> res @ head1::[] 
         | (head1::tail1, xs2, res) when tail1 = [] && check_in (xs2,head1) = true && check_in (res, head1) = true-> res
@@ -62,6 +63,9 @@ let rec intersect (xs1, xs2) =
 //40.2.4
 let rec plus (xs1, xs2) = 
     let rec fill_plus = function
+        | ([], [], res) -> res
+        | ([], head2::tail2, res) -> head2::tail2
+        | (head1::tail1, [], res) -> head1::tail1
         | (head1::tail1, head2::tail2, res) when tail1 = [] && tail2 = [] && head1 >= head2 -> res @ head2::[] @ head1::[]
         | (head1::tail1, head2::tail2, res) when tail1 = [] && tail2 = [] && head1 <= head2 -> res @ head1::[] @ head2::[]
         | (head1::tail1, head2::tail2, res) when head1 > head2 && tail2 = [] -> res @ head2::[] @ head1 :: [] @ tail1
@@ -76,20 +80,19 @@ let rec plus (xs1, xs2) =
 //4.2.5
 let rec minus (xs1, xs2) = 
     let rec fill_minus = function
+        | ([], xs2, res) -> res
         | (head1::tail1, xs2, res) when tail1 = [] && check_in (xs2,head1) = true -> res
         | (head1::tail1, xs2, res) when tail1 = [] && check_in (xs2,head1) = false-> res @ head1::[]
         | (head1::tail1, xs2, res) when check_in (xs2,head1) = true -> fill_minus(tail1, xs2, res)
         | (head1::tail1, xs2, res) when check_in (xs2,head1) = false -> fill_minus(tail1, xs2, res @ head1::[])
-    if List.length xs1 >= List.length xs2 then
-        fill_minus (xs1, xs2, [])
-    else
-        fill_minus (xs2, xs1, [])
-        
+    fill_minus (xs1, xs2, [])
+
 
 // 40.3.1
 let rec smallest = function 
-    | ((head:int)::(head1::tail)) when tail = [] && head1 <= head-> head1
-    | (head::(head1::tail)) when tail = [] && head1 >= head-> head
+    | ([]) -> None
+    | ((head:int)::(head1::tail)) when tail = [] && head1 <= head-> Some(head1)
+    | (head::(head1::tail)) when tail = [] && head1 >= head-> Some(head)
     | (head::(head1::tail)) when head > head1 -> smallest(head1::tail)
     | (head::(head1::tail)) -> smallest(head::tail)
 
@@ -97,6 +100,7 @@ let rec smallest = function
 // 40.3.2
 let rec delete (n, xs) = 
     let rec delete_min = function
+        | (n, [], res) -> res
         | ((n:int), head::tail, res) when tail = [] && head = n -> res @ tail
         | (n, head::tail, res) when tail = [] && head <> n -> res @ (head::tail)
         | (n, head::tail, res) when n = head -> res @ tail
@@ -104,8 +108,9 @@ let rec delete (n, xs) =
     delete_min(n, xs, [])
 
 let rec sort_helper = function
+    | ([], res) -> res
     | (head::tail, res) when tail = [] -> res @ head::[]
-    | (head::tail, res) -> sort_helper(delete(smallest(head::tail), head::tail), res @ smallest(head::tail)::[])
+    | (head::tail, res) -> sort_helper(delete(Option.get (smallest(head::tail)), head::tail), res @ (Option.get (smallest(head::tail)))::[])
 
 // 40.3.3
 let rec sort = fun xs ->
@@ -116,6 +121,7 @@ let min_list_rev xs:int list = List.rev xs
 // 40.4
 let rec revrev = fun (xs:int list list) ->
     let rec fill = function
+        | ([], res) -> res
         | ((head:int list)::(tail:int list list), (res:int list list)) when tail = [] -> List.rev(res @ (min_list_rev head)::[])
         | ((head:int list)::(tail:int list list), (res:int list list)) -> fill(tail, res @ (min_list_rev head)::[])
     fill(xs, [])
