6e818778113d6cba973eb6de3e0bcff8f67c6b5a
Alexandr Pirogov <88452033+eduAlexandrPirogov@users.noreply.github.com>
2022-05-05 15:37:26 +0300

    Practice with List.fold and List.foldBack

---
+++ fold.fs
@@ -0,0 +1,13 @@
+﻿// 41.4.1
+let list_filter f xs = List.foldBack(fun elem acc-> if f elem = true then elem::acc else acc@[] ) xs []
+
+// 41.4.2
+let sum (p, xs) = List.fold (fun acc elem -> if p elem = true then acc+ elem else acc+0) 0 xs
+
+// 41.4.3
+let revrev = function
+    | ([]) -> []
+    | (xs) -> List.fold (fun acc elem -> (List.fold (fun acc1 elem1 -> elem1::acc1) [] elem)::acc) [] xs
+
+revrev [[1;2];[3;4;5]] = [[5;4;3];[2;1]]
+    
